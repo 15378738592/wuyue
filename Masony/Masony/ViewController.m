87d86df51efedb5b96f7d4901175b4b468eaff52
15378738592 <mac@MacdeiMac-6.local>
2016-03-12 15:51:50 +0800

    fe
    ccd

---
+++ Masony/Masony/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  Masony
+//
+//  Created by Mac on 16/3/9.
+//  Copyright © 2016年 Mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSString *str=@"woshishui";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
